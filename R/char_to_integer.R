87575f28062046d43b95a4af1a5d4aaceef9ef99
Megan Holtorf <megan.holtorf@providence.org>
2024-04-26 14:32:47 -0700

    Fix for char_to_integer to support blanks in column.

--- R/char_to_integer.R
+++ R/char_to_integer.R
@@ -11,10 +11,11 @@ char_to_integer <- function(data) {
   char_columns <- sapply(data, is.character)
 
   for (col in names(data[char_columns])) {
-    if (all(suppressWarnings(!is.na(as.integer(data[[col]]))))) {
+    if (all(suppressWarnings(is.na(data[[col]]) | data[[col]] == "" | grepl("^\\d+$", data[[col]])))) {
       data[[col]] <- as.integer(data[[col]])
     }
   }
+  
 
   return(data)
 }
